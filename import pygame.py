e584528ee5e1c2b0c66ad91e6bf8f6b0cb988414
skenderPaenhuysen <skender.paenhuysen@ucll.be>
2025-12-15 14:25:11 +0100

    import pygame

--- import pygame.py
+++ import pygame.py
@@ -1,3 +1,14 @@
 import pygame
+
 pygame.init(
 )
+
+# Tuple representing width and height in pixels
+while True:
+    pass
+    screen_size = (1024, 768)
+
+# Create window with given size
+
+    pygame.display.set_mode(screen_size)
+ 
